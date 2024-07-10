6282561dc9a26616ce42489ce06dc4a01d821ec1
Mattieu Yves <mattieuyves@gmail.com>
2024-07-10 18:03:18 +0200

    added content home & work in progress

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,11 +9,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+
       fontFamily: {
         sans: ['Montserrat','Inter var', ...defaultTheme.fontFamily.sans],
       },
       spacing: {
         '128': '32rem',
+      },
+      backgroundImage: {
+        'noise-pattern': "url('noise.png')",
+        'form-purple': "url('svg_rand_form.svg')",
       }
     },
   },
